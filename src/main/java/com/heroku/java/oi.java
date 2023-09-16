d21b0c730cab666fbd56c59ba762b7a1367bf40e
sahandrade <92736761+sahandrade@users.noreply.github.com>
2023-09-16 04:09:50 -0300

    m

--- src/main/java/com/heroku/java/oi.java
+++ src/main/java/com/heroku/java/oi.java
@@ -1,15 +1,17 @@
-package com.heroku.java.controller;
+package com.heroku.java;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-public class user {
+@RequestMapping("/oi")
+public class oi {
     @GetMapping(value="/")
     public String getMethodName() {
-        return "hello world";
+        return "Hello world";
     }
     
 }
